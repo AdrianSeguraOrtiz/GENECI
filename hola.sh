7ddcf6cd5c9c5f8ba4f85012975d034bcc7a1d84
Adrian Segura <adrianseor.99@gmail.com>
2022-05-03 08:58:16 +0200

    update gitignore

--- hola.sh
+++ hola.sh
@@ -8,7 +8,7 @@ do
 
     for i in {1..25}
     do
-        python EAGRN-Inference.py optimize-ensemble $str --gene-names $network_folder/gene_names.txt --population-size 100 --num-evaluations 100000 --output-dir $network_folder/ea_consensus_$i
+        python EAGRN-Inference.py optimize-ensemble $str --gene-names $network_folder/gene_names.txt --population-size 100 --num-evaluations 25000 --output-dir $network_folder/ea_consensus_$i
     done
 done
 
