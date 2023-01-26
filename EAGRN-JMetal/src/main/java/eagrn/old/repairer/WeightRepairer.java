f4d8988e71a6e2770ae87d7744bf7e147092ef7c
adrian_segura <adrianseor.99@gmail.com>
2023-01-26 15:08:10 +0100

    remove old implementation

--- EAGRN-JMetal/src/main/java/eagrn/old/repairer/WeightRepairer.java
+++
@@ -1,7 +0,0 @@
-package eagrn.old.repairer;
-
-import org.uma.jmetal.solution.doublesolution.DoubleSolution;
-
-public interface WeightRepairer {
-    void repairSolution(DoubleSolution solution);
-}
