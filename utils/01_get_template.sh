424af4ab3cec07f797fc25ecf649ebe1ee45801d
adrian_segura <adrianseor.99@gmail.com>
2023-02-01 08:48:54 +0100

    improve code generalization

--- utils/01_get_template.sh
+++ utils/01_get_template.sh
@@ -2,14 +2,18 @@
 
 ## Datos de expresión
 python ../geneci/main.py extract-data expression-data \
-            --database DREAM3 --database DREAM4 --database DREAM5 --database IRMA \
+            --database DREAM3 --database DREAM4 --database DREAM5 \
+            --database IRMA --database SynTReN --database Rogers \
+            --database GeneNetWeaver \
             --username TFM-SynapseAccount \
             --password TFM-SynapsePassword \
             --output-dir ../input_data
 
 ## Gold standards
 python ../geneci/main.py extract-data gold-standard \
-            --database DREAM3 --database DREAM4 --database DREAM5 --database IRMA \
+            --database DREAM3 --database DREAM4 --database DREAM5 \
+            --database IRMA --database SynTReN --database Rogers \
+            --database GeneNetWeaver \
             --username TFM-SynapseAccount \
             --password TFM-SynapsePassword \
             --output-dir ../input_data
@@ -64,150 +68,7 @@ done
 # 3. Para las redes de tipo benchmark evaluamos la precisión de cada una de las técnicas empleadas, así como
 # de las redes consenso formadas por la media y mediana de los niveles de confianza
 
-## DREAM3
-for network_folder in ../template/*-trajectories_exp/
-do
-    mkdir -p $network_folder/measurements
-    > $network_folder/measurements/techniques.txt
-
-    id=$(echo $(basename $network_folder) | cut -d "-" -f 2)
-    size=$(echo $(basename $network_folder) | cut -d "-" -f 1)
-    size=${size#"InSilicoSize"}
-
-    num_tecs=$(ls $network_folder/lists/*.csv | wc -l)
-    weight=$(echo "scale=10; x=1/$num_tecs; if(x<1) print 0; x" | bc)
-    summands=""
-    files=""
-
-    for confidence_list in $network_folder/lists/*.csv
-    do 
-        python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                    --challenge D3C4 \
-                    --network-id ${size}_${id} \
-                    --synapse-file ../input_data/DREAM3/EVAL/PDF_InSilicoSize${size}_${id}.mat \
-                    --synapse-file ../input_data/DREAM3/EVAL/DREAM3GoldStandard_InSilicoSize${size}_${id}.txt \
-                    --confidence-list $confidence_list >> $network_folder/measurements/techniques.txt
-        summands+="--weight-file-summand $weight*$confidence_list "
-        files+="--file $confidence_list "
-    done
-
-    # Media
-    python ../geneci/main.py evaluate dream-prediction dream-weight-distribution \
-                --challenge D3C4 \
-                --network-id ${size}_${id} \
-                --synapse-file ../input_data/DREAM3/EVAL/PDF_InSilicoSize${size}_${id}.mat \
-                --synapse-file ../input_data/DREAM3/EVAL/DREAM3GoldStandard_InSilicoSize${size}_${id}.txt \
-                $summands > $network_folder/measurements/mean.txt
-    
-    # Mediana
-    python median.py $files --output-file "./temporal_list.csv"
-    python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                --challenge D3C4 \
-                --network-id ${size}_${id} \
-                --synapse-file ../input_data/DREAM3/EVAL/PDF_InSilicoSize${size}_${id}.mat \
-                --synapse-file ../input_data/DREAM3/EVAL/DREAM3GoldStandard_InSilicoSize${size}_${id}.txt \
-                --confidence-list "./temporal_list.csv" > $network_folder/measurements/median.txt
-    rm "./temporal_list.csv"
-done
-
-## DREAM4
-for network_folder in ../template/dream4*_exp/
-do
-    mkdir -p $network_folder/measurements
-    > $network_folder/measurements/techniques.txt
-
-    id=$(echo $(basename $network_folder) | cut -d "_" -f 3)
-    id=${id#"0"}
-    size=$(echo $(basename $network_folder) | cut -d "_" -f 2)
-    size=${size#"0"}
-
-    num_tecs=$(ls $network_folder/lists/*.csv | wc -l)
-    weight=$(echo "scale=10; x=1/$num_tecs; if(x<1) print 0; x" | bc)
-    summands=""
-    files=""
-
-    for confidence_list in $network_folder/lists/*.csv
-    do 
-        python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                    --challenge D4C2 \
-                    --network-id ${size}_${id} \
-                    --synapse-file ../input_data/DREAM4/EVAL/pdf_size${size}_${id}.mat \
-                    --confidence-list $confidence_list >> $network_folder/measurements/techniques.txt
-        summands+="--weight-file-summand $weight*$confidence_list "
-        files+="--file $confidence_list "
-    done
-
-    # Media
-    python ../geneci/main.py evaluate dream-prediction dream-weight-distribution \
-                --challenge D4C2 \
-                --network-id ${size}_${id} \
-                --synapse-file ../input_data/DREAM4/EVAL/pdf_size${size}_${id}.mat \
-                $summands > $network_folder/measurements/mean.txt
-    
-    # Mediana
-    python median.py $files --output-file "./temporal_list.csv"
-    python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                --challenge D4C2 \
-                --network-id ${size}_${id} \
-                --synapse-file ../input_data/DREAM4/EVAL/pdf_size${size}_${id}.mat \
-                --confidence-list "./temporal_list.csv" > $network_folder/measurements/median.txt
-    rm "./temporal_list.csv"
-done
-
-## DREAM5
-for network_folder in ../template/net*_exp/
-do
-    mkdir -p $network_folder/measurements
-    > $network_folder/measurements/techniques.txt
-
-    id=$(basename $network_folder)
-    id=${id#"net"}
-    id=${id%"_exp"}
-
-    num_tecs=$(ls $network_folder/lists/*.csv | wc -l)
-    weight=$(echo "scale=10; x=1/$num_tecs; if(x<1) print 0; x" | bc)
-    summands=""
-    files=""
-
-    for confidence_list in $network_folder/lists/*.csv
-    do 
-        python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                    --challenge D5C4 \
-                    --network-id $id \
-                    --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_Edges_Network${id}.tsv \
-                    --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_GoldStandard_Network${id}.tsv \
-                    --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUPR.mat \
-                    --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUROC.mat \
-                    --confidence-list $confidence_list >> $network_folder/measurements/techniques.txt
-        summands+="--weight-file-summand $weight*$confidence_list "
-        files+="--file $confidence_list "
-    done
-
-    # Media
-    python ../geneci/main.py evaluate dream-prediction dream-weight-distribution \
-                --challenge D5C4 \
-                --network-id $id \
-                --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_Edges_Network${id}.tsv \
-                --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_GoldStandard_Network${id}.tsv \
-                --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUPR.mat \
-                --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUROC.mat \
-                $summands > $network_folder/measurements/mean.txt
-    
-    # Mediana
-    python median.py $files --output-file "./temporal_list.csv"
-    python ../geneci/main.py evaluate dream-prediction dream-list-of-links \
-                --challenge D5C4 \
-                --network-id $id \
-                --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_Edges_Network${id}.tsv \
-                --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_GoldStandard_Network${id}.tsv \
-                --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUPR.mat \
-                --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUROC.mat \
-                --confidence-list "./temporal_list.csv" > $network_folder/measurements/median.txt
-    rm "./temporal_list.csv"
-done
-
-## IRMA
-for network_folder in ../template/switch-*_exp/
+for network_folder in ../template/*/
 do
     mkdir -p $network_folder/measurements
     > $network_folder/measurements/techniques.txt
@@ -217,24 +78,75 @@ do
     summands=""
     files=""
 
+    base=$(basename $network_folder)
+    if [[ $base =~ [*-trajectories_exp] ]]
+    then
+        dream=true
+        id=$(echo $base | cut -d "-" -f 2)
+        size=$(echo $base | cut -d "-" -f 1)
+        size=${size#"InSilicoSize"}
+
+        challenge="D3C4"
+        network_id="${size}_${id}"
+        eval_files_str="--synapse-file ../input_data/DREAM3/EVAL/PDF_InSilicoSize${size}_${id}.mat --synapse-file ../input_data/DREAM3/EVAL/DREAM3GoldStandard_InSilicoSize${size}_${id}.txt"
+
+    elif [[ $base =~ [dream4*_exp] ]]
+    then
+        dream=true
+        id=$(echo $base | cut -d "_" -f 3)
+        id=${id#"0"}
+        size=$(echo $base | cut -d "_" -f 2)
+        size=${size#"0"}
+
+        challenge="D4C2"
+        network_id="${size}_${id}"
+        eval_files_str="--synapse-file ../input_data/DREAM4/EVAL/pdf_size${size}_${id}.mat"
+
+    elif [[ $base =~ [net*_exp] ]]
+    then
+        dream=true
+        id=${base#"net"}
+        id=${id%"_exp"}
+
+        challenge="D5C4"
+        network_id="$id"
+        eval_files_str="--synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_Edges_Network${id}.tsv --synapse-file ../input_data/DREAM5/EVAL/DREAM5_NetworkInference_GoldStandard_Network${id}.tsv --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUPR.mat --synapse-file ../input_data/DREAM5/EVAL/Network${id}_AUROC.mat"
+
+    elif [[ $base =~ [switch-*_exp] ]]
+    then
+        dream=false
+        gs="../input_data/IRMA/GS/irma_gs.csv"
+    else
+        dream=false
+        name=${base%"_exp"}
+        gs=$(ls ../input_data/*/GS/${name}_gs.csv)
+    fi
+
+    if [ "$dream" = true ]
+    then
+        tag="dream"
+        flags="--challenge $challenge --network-id $network_id $eval_files_str"
+    else
+        tag="generic"
+        flags="--gs-binary-matrix $gs"
+    fi
+
     for confidence_list in $network_folder/lists/*.csv
     do 
-        python ../geneci/main.py evaluate generic-prediction generic-list-of-links \
-                    --gs-binary-matrix ./../input_data/IRMA/GS/irma_gs.csv \
+        python ../geneci/main.py evaluate ${tag}-prediction ${tag}-list-of-links $flags \
                     --confidence-list $confidence_list >> $network_folder/measurements/techniques.txt
         summands+="--weight-file-summand $weight*$confidence_list "
         files+="--file $confidence_list "
     done
 
     # Media
-    python ../geneci/main.py evaluate generic-prediction generic-weight-distribution \
-                --gs-binary-matrix ./../input_data/IRMA/GS/irma_gs.csv \
+    python ../geneci/main.py evaluate ${tag}-prediction ${tag}-weight-distribution $flags \
                 $summands > $network_folder/measurements/mean.txt
     
     # Mediana
     python median.py $files --output-file "./temporal_list.csv"
-    python ../geneci/main.py evaluate generic-prediction generic-list-of-links \
-                --gs-binary-matrix ./../input_data/IRMA/GS/irma_gs.csv \
+    python ../geneci/main.py evaluate ${tag}-prediction ${tag}-list-of-links $flags \
                 --confidence-list "./temporal_list.csv" > $network_folder/measurements/median.txt
     rm "./temporal_list.csv"
+
 done
